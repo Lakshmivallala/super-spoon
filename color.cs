399f0931535d271a23547abb386bec9c8014762d
EpperlaKarthik <srikarthik0210@gmail.com>
2019-07-14 08:00:56 +0530

    VS
    Colour changing script

---
+++ color.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Color : MonoBehaviour
+{
+
+    public Material brown;
+    public Material Red;
+    public Material purple;
+    public Material pink;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+
+    // changes colour to brown
+    public void changeColortobrown()
+    {
+        GetComponent<Renderer>().material = brown;
+    }
+    // changes colour to red
+    public void changeColortored()
+    {
+        GetComponent<Renderer>().material = Red;
+    }
+    // changes colour to pink
+    public void changeColortopink()
+    {
+        GetComponent<Renderer>().material = pink;
+    }
+    // changes colour to purple
+    public void changeColortopurple()
+    {
+        GetComponent<Renderer>().material = purple;
+    }
+}
+
+
